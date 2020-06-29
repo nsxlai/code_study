8a9231f47ec869eff31187f92ed3c5722e134d06
Ray Lai (SkyNet) <nsxlai2000@gmail.com>
2020-06-29 07:47:42 -0700

    Add new HackerRank practice example

---
+++ HackerRank/08_hasmap_two_strings.py
@@ -0,0 +1,16 @@
+def twoStrings(s1, s2):
+    set1 = set(s1)
+    set2 = set(s2)
+    if set1 & set2 != set():
+        result = 'YES'
+    else:
+        result = 'NO'
+    return result
+
+
+if __name__ == '__main__':
+    s1 = 'hello'
+    s2 = 'world'
+    result = twoStrings(s1, s2)
+    print(result)
+    assert result == 'YES'  # 'o' and 'l' are in both string
