764b2989187f235dc8c7c3e2bd10aa01a3d859ab
Ray Lai (SkyNet) <nsxlai2000@gmail.com>
2020-08-01 16:20:45 -0700

    Summary 1. Move python-mock-01 under PYTEST 2. Add Golang code to the repo 3. Add api study code under PYTHON FUNC STUDY

---
+++ PYTHON_FUNC_STUDY/api/covid19_example.py
@@ -0,0 +1,16 @@
+# source: https://towardsdatascience.com/json-and-apis-with-python-a3282aa75963
+import requests
+from pprint import pprint
+import pandas as pd
+
+
+if __name__ == '__main__':
+    url = 'https://api.covid19api.com/summary'
+    response = requests.get(url).json()
+    print(response.keys())
+    country_list = [detail['Country'] for detail in response['Countries']]
+    # pprint(country_list)
+    # pprint(response['Countries'])
+    covid19_world = pd.DataFrame(response['Countries'])
+    print(covid19_world.iloc[177])  # USA
+
