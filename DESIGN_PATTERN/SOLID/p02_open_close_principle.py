a35f301d7e857cde18c7129603d91c4adc4ac697
Ray Lai (WindowsPC-JAS) <nsxlai2000@gmail.com>
2020-08-15 14:15:00 -0700

    Add Python open-close example code under SOLID. Convert the file from JavaScript to Python

--- DESIGN_PATTERN/SOLID/p02_open_close_principle.py
+++ DESIGN_PATTERN/SOLID/p02_open_close_principle.py
@@ -3,137 +3,81 @@
 #
 # Also use the RealPython tutoral on interface to build the meta class interface
 # https://realpython.com/python-interface/
+# The follow code was originally written in JavaScript but translate it into Python
+from abc import ABC, abstractmethod
+from random import randint
+from typing import List, Union
 
-import abc
 
-class Interface(abc.ABC):
-    @abc.abstractmethod
-    def monster(self):
+class MonsterInterface(ABC):
+    @abstractmethod
+    def __init__(self):
         pass
 
-def createWithInterfaceValidation(prototypeObject, interfaceObject):
+    @abstractmethod
+    def rampage(self, location):
+        pass
 
+
+class Kaiju(MonsterInterface):
+    def __init__(self, name):
+        self.name = name
+        self.type = "Kaiju"
+
+    def rampage(self, location):
+        print(f'The {self.type} {self.name} is rampaging through {location}!')
+
+
+class GreatOldOne(MonsterInterface):
+    def __init__(self, name):
+        self.name = name
+        self.type = "Great Old One"
+
+    def rampage(self, location):
+        print(f'The {self.type} {self.name} has awaken from its slumber in {location}!')
+
+
+class MythicalMonster(MonsterInterface):
+    def __init__(self, name):
+        self.name = name
+        self.type = "Mythical Monster"
+
+    def rampage(self, location):
+        print(f'The {self.type} {self.name} has been sighted in {location}!')
+
+
+class MonsterManager:
+    """ Monster Types and Manager """
+    def __init__(self, monsters: List[MonsterInterface], locations: List[str]):
+        self.monsters = monsters
+        self.locations = locations
+
+    def getRandomLocation(self):
+        return self.locations[randint(0, len(locations)-1)]
+
+    def rampageAll(self):
+        for monster in monsters:
+            location = self.getRandomLocation()
+            monster.rampage(location)
 
 
 if __name__ == '__main__':
+    """
+    The key to open-close principle, in this example, the MonsterManager does not have to change. To create
+    a new type of monster, just inherit the MonsterInterface object and create a new monster type. The MonsterManager
+    class does not have to change at all
+    """
     monsters = []
     locations = ["Athens", "Budapest", "New York", "Santiago", "Tokyo"]
 
+    rodan = Kaiju('Rodan')
+    monsters.append(rodan)
 
+    gzxtyos = GreatOldOne('Gzxtyos')
+    monsters.append(gzxtyos)
 
-'''
-// Interface Approximation Utilities
-function ImplementationError(message) {
-  this.name = "ImplementationError";
-  this.message = message;
-}
-ImplementationError.prototype = new Error();
+    cerberus = MythicalMonster('Cerberus')
+    monsters.append(cerberus)
 
-function createWithInterfaceValidation(prototypeObject, interfaceObject) {
-  Object.keys(interfaceObject).forEach(function(key) {
-    if (prototypeObject[key] === null || typeof prototypeObject[key] !== "function") {
-      throw new ImplementationError(
-        "Required method " + key + " has not been implemented."
-      );
-    }
-  });
-
-  return Object.create(prototypeObject);
-}
-
-// Monster Types and Manager
-var MonsterManager = {
-  init: function(monsters, locations) {
-    this.monsters = monsters;
-    this.locations = locations;
-  },
-
-  getRandomLocation: function() {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
-
-    return this.locations[getRandomInt(this.locations.length)];
-  },
-
-  rampageAll: function() {
-    this.monsters.forEach(function(monster) {
-      var location = this.getRandomLocation();
-
-      monster.rampage(location);
-    }, this);
-  }
-};
-
-var MonsterInterface = {
-  init: null,
-  rampage: null,
-};
-
-var Kaiju = Object.create(MonsterInterface);
-Kaiju.init = function(name) {
-  this.name = name;
-  this.type = "Kaiju";
-
-  return this;
-};
-Kaiju.rampage = function(location) {
-  console.log(
-    "The " + this.type + " " + this.name +
-    " is rampaging through " + location + "!"
-  );
-};
-
-var GreatOldOne = Object.create(MonsterInterface);
-GreatOldOne.init = function(name) {
-  this.name = name;
-  this.type = "Great Old One";
-
-  return this;
-};
-GreatOldOne.rampage = function(location) {
-  console.log(
-    "The " + this.type + " " + this.name +
-    " has awaken from its slumber in " + location + "!"
-  );
-};
-
-var MythicalMonster = Object.create(MonsterInterface);
-MythicalMonster.init = function(name) {
-  this.name = name;
-  this.type = "Mythical Monster";
-
-  return this;
-};
-MythicalMonster.rampage = function(location) {
-  console.log(
-    "The " + this.type + " " + this.name +
-    " has been sighted in " + location + "!"
-  );
-};
-
-// Rampage!
-var monsters = [];
-var locations = ["Athens", "Budapest", "New York", "Santiago", "Tokyo"];
-
-var rodan = createWithInterfaceValidation(Kaiju, MonsterInterface);
-rodan.init("Rodan");
-monsters.push(rodan);
-
-var gzxtyos = createWithInterfaceValidation(GreatOldOne, MonsterInterface);
-gzxtyos.init("Gzxtyos");
-monsters.push(gzxtyos);
-
-var cerberus = createWithInterfaceValidation(MythicalMonster, MonsterInterface);
-cerberus.init("Cerberus");
-monsters.push(cerberus);
-
-var myMonsterManager = Object.create(MonsterManager);
-myMonsterManager.init(monsters, locations);
-
-myMonsterManager.rampageAll();
-  // Logs: (with variable city names)
-    // The Kaiju Rodan is rampaging through Tokyo!
-    // The Great Old One Gzxtyos has awaken from its slumber in Athens!
-    // The Mythical Monster Cerberus has been sighted in New York!
-'''
+    myMonsterManager = MonsterManager(monsters, locations)
+    myMonsterManager.rampageAll()
