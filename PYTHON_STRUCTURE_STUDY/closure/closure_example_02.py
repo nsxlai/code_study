8a6a376ebeb688a7c8d8f4aff62ffc0439ec747f
Ray Lai <nsxlai2000@gmail.com>
2021-05-12 21:38:16 -0700

    Add closure example

--- PYTHON_STRUCTURE_STUDY/closure/closure_example_02.py
+++ PYTHON_STRUCTURE_STUDY/closure/closure_example_02.py
@@ -1,44 +1,25 @@
-# source: https://towardsdatascience.com/dont-use-recursion-in-python-any-more-918aad95094c
-from dataclasses import dataclass
-from typing import Callable
+"""
+source: https://medium.com/python-features/introduction-to-closures-in-python-8d697ff9e44d
+
+When there are few methods (one method in most cases) to be implemented in a class,
+closures can provide an alternate and more elegant solution. Also, closures and nested
+functions are especially useful if we want to write code based on the concept of lazy
+or delayed evaluation
+"""
 
 
-students = {
-    'Alice': 98,
-    'Bob': 67,
-    'Chris': 85,
-    'David': 75,
-    'Ella': 54,
-    'Fiona': 35,
-    'Grace': 69,
-    'Mary': 91,
-    'Lily': 82,
-    'John': 73,
-    'Mason': 30,
-    'Eaton': 45,
-    'Zack': 95,
-}
+def countdown(start):
+    # This is the outer enclosing function
+    def display():
+        # This is the nested function
+        n = start
+        while n > 0:
+            n -= 1
+            print('T-minus %d' % n)
 
-
-def make_student_classifier(lower_bound, upper_bound):
-    def classify_student(exam_dict):
-        return {k: v for (k, v) in exam_dict.items() if lower_bound <= v < upper_bound}
-    return classify_student
-
-
-@dataclass
-class grade:
-    grade_A: Callable = make_student_classifier(90, 100)
-    grade_B: Callable = make_student_classifier(80, 90)
-    grade_C: Callable = make_student_classifier(70, 80)
-    grade_D: Callable = make_student_classifier(60, 70)
-    grade_F: Callable = make_student_classifier(0, 60)
+    return display
 
 
 if __name__ == '__main__':
-    s_grade = grade()
-    print(f'{s_grade.grade_A(students) = }')
-    print(f'{s_grade.grade_B(students) = }')
-    print(f'{s_grade.grade_C(students) = }')
-    print(f'{s_grade.grade_D(students) = }')
-    print(f'{s_grade.grade_F(students) = }')
+    counter1 = countdown(3)
+    counter1()
