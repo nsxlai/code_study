ccb444b334a224a98f840b04d48c8b05360c75a3
Ray Lai <nsxlai2000@gmail.com>
2021-05-18 21:56:24 -0700

    Update custom_user/creational_pattern/singleton_03.py example with comment from Medium articl for __new__ and __init__ interaction
    Create PROJECT_CONCEPT folder and move RPI and RPI_Cluster examples under

--- DESIGN_PATTERN/Python Design Patterns/99_Custom_User_Case/creational_pattern/singleton_03.py
+++ DESIGN_PATTERN/Python Design Patterns/99_Custom_User_Case/creational_pattern/singleton_03.py
@@ -1,4 +1,34 @@
-# source: Medium article "3 Levels of Understanding the Singleton Pattern in Python"
+"""
+source: Medium article "3 Levels of Understanding the Singleton Pattern in Python"
+
+Comment structure source: https://medium.com/python-features/instance-creation-in-python-9268d320f0a
+We haven’t defined dunder new in our Employee class but we do inherit an implementation from the universal
+base class object. So it is the base class implementation of dunder new which is responsible for allocating
+our object in this case.
+
+Hence, inherited __new__() allocates the object which is passed to the __init__() constructor as self.
+Now, let’s try to override dunder new method to understand its signature better. We’ll implement the most
+basic override of dunder new, which simply delegates to base class implementation, along with adding few
+print statements, to inspect the arguments and return value.
+
+The first thing to note is, dunder new is a class method. It expects cls as its first argument rather than
+self. The cls argument is the class of the new object, which will be allocated. In addition, dunder new
+accepts whatever parameters have been passed to the constructor. In this case, we have used the *args and **kwargs
+for the same.
+
+The main purpose of dunder new is to allocate the instance of the calling class. All object
+allocation must be done by the dunder new implementation on the ultimate base class object. We’ve used super()
+to refer the object new method since that is more maintainable. (if ultimate base class object changes in future).
+Finally, we are returning the newly created object obj which is passed to the __init__ method as self. This can
+be confirmed by comparing the ids of both objects.
+
+Summary:
+1. dunder new __new__() allocates and returns new instances. It is an implicit class method that accepts
+class of the new instance as its first argument.
+2. object.__new__() is the ultimate allocator which allocates all instances.
+3. dunder init __init__ is responsible for mutating the instance it has been given.
+"""
+
 
 class Singleton:
     __instance = None  # double underscore (__) will put the attribute as "private" in Python
