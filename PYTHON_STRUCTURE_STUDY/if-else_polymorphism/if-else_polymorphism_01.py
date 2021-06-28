4da1da4583f34f32154f102175b19e8e46a2f7b2
Ray Lai <nsxlai2000@gmail.com>
2021-06-27 20:20:04 -0700

    Complete if-else_polymorphism_01.py example code and also add the if-else_dict_pattern.py example code

--- PYTHON_STRUCTURE_STUDY/if-else_polymorphism/if-else_polymorphism_01.py
+++ PYTHON_STRUCTURE_STUDY/if-else_polymorphism/if-else_polymorphism_01.py
@@ -19,35 +19,42 @@ def Accept():
         # do default action
 
 Will refactor the code above to state machine using polymorphism (avoiding using if-else)
+
+On the surface, this example is very close to the state machine; however, state machine requires all the states to
+be defined in the beginning of the programming and follow a specific program run decision pattern (yes/no). Like
+data storage tape. This code example does not require all the state to be defined at run time but rather user input
+define what the next state is going to be.
 """
 from datetime import datetime
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class BookingStateBase(ABC):
     @abstractmethod
-    def EnterState(self, booking: Booking):
+    def EnterState(self, booking: Any):
         pass
 
     @abstractmethod
-    def Cancel(self, booking: Booking, reason: str):
+    def Cancel(self, booking: Any, reason: str):
         pass
 
     @abstractmethod
-    def Accept(self, booking: Booking):
+    def Accept(self, booking: Any):
         pass
 
 
 class Booking:
     def __init__(self, pid: str, attendeeName: str, state: BookingStateBase):
         self.pid = pid
-        self.stateBase = state
+        self.state = state
         self.AttendeeName = attendeeName
         self.BookingDate = datetime.now()
 
     @classmethod
     def createNew(cls, pid: str, attendeeName: str):
-        cls.state = ProcessedState()
+        cls.state = PendingState()  # Create new and immediately put into PendingState
+        cls.state.EnterState(cls)
         return cls(pid, attendeeName, cls.state)
 
     def TransitionToState(self, newState: BookingStateBase):
@@ -55,52 +62,83 @@ class Booking:
         self.state.EnterState(self)
 
     def Accept(self):
-        # this.state.Accept(this)
-        pass
+        # Transition to the next state with Accept path
+        self.state.Accept(self)
 
     def Cancel(self, cancellationReason: str):
-        # this.state.Cancel(this, cancellationReason)
-        pass
+        # Transition to the next state with Accept path
+        self.state.Cancel(self, cancellationReason)
 
 
 class PendingState(BookingStateBase):
+    order_check = False
+
     def EnterState(self, booking: Booking):
-        print('PendingState: EnterState')
+        print('Enter => PendingState')
+        print('Initiate Order check...')
+        self.order_check = True  # In realistic development, someone input hook will be here to update order_check
 
     def Cancel(self, booking: Booking, reason: str):
-        print('PendingState: CancelState')
+        print('PendingState => Cancel')
+        print(f'Cancellation reason: {reason}')
         booking.TransitionToState(CancelledState())
 
     def Accept(self, booking: Booking):
-        print('PendingState: AcceptState')
-        booking.TransitionToState(ProcessedState())
+        if self.order_check:
+            print('PendingState => Accept')
+            booking.TransitionToState(ProcessedState())
+        else:
+            print('PendingState => Cancel')
+            booking.TransitionToState(CancelledState())
 
 
 class ProcessedState(BookingStateBase):
     def EnterState(self, booking: Booking):
-        print('ProcessState: EnterState')
+        print('Enter => ProcessedState')
+        print(f'PID = {booking.pid}')
 
     def Cancel(self, booking: Booking, reason: str):
-        print('ProcessState: CancelState')
+        print('ProcessState => Cancel')
+        print(f'PID = {booking.pid}')
+        print(f'Cancellation reason: {reason}')
         booking.TransitionToState(CancelledState())
 
     def Accept(self, booking: Booking):
-        print('ProcessState: AcceptState')
-        booking.TransitionToState(ProcessedState())
+        print('ProcessState => Accept')
+        print(f'PID = {booking.pid}')
+        print('End of state; no more transition')
 
 
 class CancelledState(BookingStateBase):
     def EnterState(self, booking: Booking):
-        print('CancelledState: EnterState')
+        print('Enter => CancelledState')
+        print(f'PID = {booking.pid}')
 
     def Cancel(self, booking: Booking, reason: str):
-        print('CancelledState: CancelState')
-        booking.TransitionToState(CancelledState())
+        # This method is there but not used because of the interface requirement. Cancel the cancelledState
+        # doesn't reverse back to either pendingState or processedState
+        print('CancelledState => Cancel')
+        print(f'PID = {booking.pid}')
+        print(f'Cancellation reason: {reason}')
 
     def Accept(self, booking: Booking):
-        print('CancelledState: AcceptState')
-        booking.TransitionToState(ProcessedState())
+        # The cancel is accepted
+        print('CancelledState => Accept')
+        print(f'PID = {booking.pid}')
+        print('End of state; no more transition')
 
 
 if __name__ == '__main__':
+    print(' Flow 1 '.center(40, '-'))
     nb = Booking.createNew(pid='001', attendeeName='John')
+    nb.Accept()  # Accept the Pending state and move to Processed State
+    nb.Accept()  # Accept the Processed state and reach end of the flow
+
+    print(' Flow 2 '.center(40, '-'))
+    nb = Booking.createNew(pid='002', attendeeName='Mary')
+    nb.Accept()
+    nb.Cancel('No longer needed')
+
+    print(' Flow 3 '.center(40, '-'))
+    nb = Booking.createNew(pid='003', attendeeName='Lisa')
+    nb.Cancel('No longer needed')
