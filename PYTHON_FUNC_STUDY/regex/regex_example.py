2d233613c75784541a28b2286289d2f94e988aed
Ray Lai <ray.lai@getcruise.com>
2021-11-12 19:25:05 -0800

    Add regex example

---
+++ PYTHON_FUNC_STUDY/regex/regex_example.py
@@ -0,0 +1,31 @@
+import re
+
+
+if __name__ == '__main__':
+    input_str = '[)>RS06GSY8161302000000XGSP1X391433AGS12V000000000GST1121045012RSEOT'
+
+    # Method 1
+    vpps = re.search(r'GSY(\w+)XGS', input_str)
+    print(f'{vpps.group(0) = }')
+    print(f'{vpps.group(1) = }')
+    vpps = vpps.group(1)
+
+    # Method 2
+    pattern = re.compile(r'GSP(\w+)GS12V')
+    pn = pattern.search(input_str)
+    print(f'{pn.group(0) = }')
+    print(f'{pn.group(1) = }')
+    pn = pn.group(1)
+
+    duns = re.search(r'12V(\w+)GST', input_str)
+    print(f'{duns.group(0) = }')
+    print(f'{duns.group(1) = }')
+    duns = duns.group(1)
+
+    sn = re.search(r'GST(\w+)RSEOT', input_str)
+    print(f'{sn.group(0) = }')
+    print(f'{sn.group(1) = }')
+    sn = sn.group(1)
+
+    product_info = {'vpps': vpps, 'pn': pn, 'duns': duns, 'sn': sn}
+    print(f'{product_info = }')
