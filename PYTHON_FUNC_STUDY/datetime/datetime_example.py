12f3783d60c8d6f40cf0a8f6180c35064b4c40d0
Ray Lai <ray.lai@getcruise.com>
2022-03-21 21:17:58 -0700

    Update datetime example code

--- PYTHON_FUNC_STUDY/datetime/datetime_example.py
+++ PYTHON_FUNC_STUDY/datetime/datetime_example.py
@@ -1,12 +1,25 @@
 """
 The "dayOfTheWeek" function is sourced from CODING_PLATFORM/leetcode/mock_interview/q03_find_date.py
 """
-from datetime import datetime
+from datetime import datetime, date
 
 
 def dayOfTheWeek(day: int, month: int, year: int) -> str:
     out = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
     return out[datetime(year, month, day).weekday()]
+
+
+def get_datetime_str():  # Adding this here until Stoa is requirement
+    """Get the date time string : YYYYMMDDHHMMSS.
+    """
+    date_object = date.today()
+    current_date = str(date_object).replace('-', '')
+
+    time_object = datetime.now()
+    current_time = time_object.strftime("%H%M%S")
+
+    datetime_str = (str(current_date) + str(current_time))
+    return datetime_str
 
 
 if __name__ == '__main__':
@@ -26,3 +39,6 @@ if __name__ == '__main__':
     print(f'Today is {current:%B} {current:%d}, {current:%Y}')  # capital %Y will display to full 4-digial year format
     print(f'Today is {current:%m}/{current:%d}/{current:%y}')  # lower case %y will just show 2-digital year format
     print(f'Current time is {current:%H}:{current:%M}:{current:%S}')
+
+    # Example 4
+    print(f'{get_datetime_str() = }')
